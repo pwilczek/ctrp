2bb76263836af2073de8a07da04f9250b1149859
Piotr Wilczek <piotr.wilczek@wilu.name>
2022-04-15 10:02:22 +0200

    added test

---
+++ src/test/java/name/wilu/ctrp/AppRunnerTest.java
@@ -0,0 +1,12 @@
+package name.wilu.ctrp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppRunnerTest {
+    @Test
+    public void weReOk() {
+        assertTrue(1 == 1);
+    }
+}
